0591133be1361a0a5532de3f775c2ed4209eafaa
honey <dyh@163.com>
2021-04-02 19:22:00 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+      <p>java0923 全体同学 行走的offer收割机</p>
+      <p>java0923 全体同学 争取第一周入职 第二周转正,第三周升为组长,第四周董事会</p>
+      <p>祝 java0923 全体同学 拿到自己心仪的offer</p>
+      <p>祝 java0923 全体同学 拿到自己心仪的offer</p>
+   <img src="http://img12.360buyimg.com/n1/s450x450_jfs/t1/142295/6/19866/63874/5fe4408eE401889c8/8f5c2b37676a2264.jpg"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
