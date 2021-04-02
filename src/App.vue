b444f57702df2841e545a2a80eb362327e5f9861
honey <honey@163.com>
2021-04-02 21:32:24 +0800

    第二次修改

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
       <p>java0923 hello word</p>
-      <p>java0923 全体同学 争取第一周入职 第二周转正,第三周升为组长,第四周董事会</p>
-      <p>祝 java0923 全体同学 拿到自己心仪的offer</p>
+      <p>java0923 hello world</p>
+      <p>祝 java0923 hello world</p>
       <p>祝 java0923 全体同学 拿到自己心仪的offer</p>
    <img src="http://img12.360buyimg.com/n1/s450x450_jfs/t1/142295/6/19866/63874/5fe4408eE401889c8/8f5c2b37676a2264.jpg"/>
   </div>
