b77a19f2c314ed430622bc8400cb9911977a6520
honey <dyh@163.com>
2021-04-02 22:37:07 +0800

    添加了很多hello world和两张图片

--- src/App.vue
+++ src/App.vue
@@ -15,8 +15,9 @@
       <p>祝 java0923 hello world</p>
       <p>祝 java0923 全体同学 拿到自己心仪的offer</p>
    <img src="http://img12.360buyimg.com/n1/s450x450_jfs/t1/142295/6/19866/63874/5fe4408eE401889c8/8f5c2b37676a2264.jpg"/>
-   <img src="https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3681880960,455182084&fm=26&gp=0.jpg"/>
-  </div>
+   <img src="http://img12.360buyimg.com/n1/s450x450_jfs/t1/142295/6/19866/63874/5fe4408eE401889c8/8f5c2b37676a2264.jpg"/>
+   <img src="http://img12.360buyimg.com/n1/s450x450_jfs/t1/142295/6/19866/63874/5fe4408eE401889c8/8f5c2b37676a2264.jpg"/>
+ </div>
 </template>
 
 <script>
